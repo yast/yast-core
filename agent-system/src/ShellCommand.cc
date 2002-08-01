aa8232aa525816cd9f8c03a7bc57e9e076225ea7
Arvin Schnell <arvin@suse.de>
2002-08-01 09:54:16 +0000

    - used _exit instead of exit (for VNC installation by meissner) - coding style
    svn path=/trunk/core/; revision=4134

--- agent-system/src/ShellCommand.cc
+++ agent-system/src/ShellCommand.cc
@@ -23,157 +23,186 @@
 /**
  * Execute shell command and feed its output to y2log
  */
-int shellcommand(const string &command, const string &tempdir)
+int
+shellcommand (const string &command, const string &tempdir)
 {
     y2debug ("shellcommand start");
 
     int ret = 0;
 
     int pipe1[2];
-    if(pipe(pipe1)) {
+    if (pipe (pipe1))
+    {
 	y2error ("pipe failed");
 	return -1;
     }
 
-    fflush(0);
-    pid_t log2 = fork();
+    fflush (0);
+    pid_t log2 = fork ();
 
-    /* y2log stderr */
-    if(log2) {
+    if (log2)
+    {
+	/* y2log stderr */
 
 	FILE *err = NULL;
-	if(!tempdir.empty ())
-	    err = fopen((tempdir+"/stderr").c_str(), "w");
+	if (!tempdir.empty ())
+	    err = fopen ((tempdir + "/stderr").c_str (), "w");
 
-	char* s1 = (char*)malloc(sizeof(char*)*1024);
-	if(s1 == NULL) {
-	    y2error("s1 malloc error");
+	char *s1 = (char *) malloc (sizeof (char *) * 1024);
+	if (s1 == NULL)
+	{
+	    y2error ("s1 malloc error");
 	    return -1;
 	}
-	FILE* stream1 = fdopen(pipe1[0], "r");
-	if(stream1 == NULL) {
-	    y2error("stream1 fdopen error");
-	    free(s1);
+
+	FILE *stream1 = fdopen (pipe1[0], "r");
+	if (stream1 == NULL)
+	{
+	    y2error ("stream1 fdopen error");
+	    free (s1);
 	    return -1;
 	}
-	close(pipe1[1]);
+	close (pipe1[1]);
 
-	while(fgets(s1,1024,stream1)) {
-	    if(err) fprintf(err, "%s", s1);
+	while (fgets (s1, 1024, stream1))
+	{
+	    if (err)
+		fprintf (err, "%s", s1);
 	    // yes, this should be y2error but some external
 	    // programs print normal messages via stderr
 	    // bug reports are filed but who knows if such programs
 	    // ever get fixed.
-	    y2warning("%s", s1);
+	    y2warning ("%s", s1);
 	}
 
-	fclose(stream1);
-	free(s1);
+	fclose (stream1);
+	free (s1);
 
-	if(err)
-	    fclose(err);
-
+	if (err)
+	    fclose (err);
     }
-    /* y2log stdout */
-    else {
+    else
+    {
+	/* y2log stdout */
 
-	dup2(pipe1[1], 2);
-	close(pipe1[0]);
-	close(pipe1[1]);
+	dup2 (pipe1[1], 2);
+	close (pipe1[0]);
+	close (pipe1[1]);
 
 	int pipe2[2];
-	if(pipe(pipe2)) {
+	if (pipe (pipe2))
+	{
 	    y2error ("pipe failed");
-	    exit(1);
+	    _exit (1);
 	}
 
-	fflush(0);
-	pid_t child = fork();
+	fflush (0);
+	pid_t child = fork ();
 
-	/* y2log stdout */
-	if(child) {
+	if (child)
+	{
+	    /* y2log stdout */
 
 	    FILE *out = NULL;
-	    if(!tempdir.empty ())
-		out = fopen((tempdir+"/stdout").c_str(), "w");
+	    if (!tempdir.empty ())
+		out = fopen ((tempdir + "/stdout").c_str (), "w");
 
-	    char* s2 = (char*)malloc(sizeof(char*)*1024);
-	    if(s2 == NULL) {
-		y2error("s2 malloc error");
-		exit(1);
-	    }
-	    FILE* stream2 = fdopen(pipe2[0], "r");
-	    if(stream2 == NULL) {
-		y2error("stream2 fdopen error");
-		free(s2);
-		exit(1);
-	    }
-	    close(pipe2[1]);
-
-	    while(fgets(s2,1024,stream2)) {
-		if(out) fprintf(out, "%s", s2);
-		y2debug("%s", s2);
+	    char *s2 = (char *) malloc (sizeof (char *) * 1024);
+	    if (s2 == NULL)
+	    {
+		y2error ("s2 malloc error");
+		_exit (1);
 	    }
 
-	    fclose(stream2);
-	    free(s2);
+	    FILE *stream2 = fdopen (pipe2[0], "r");
+	    if (stream2 == NULL)
+	    {
+		y2error ("stream2 fdopen error");
+		free (s2);
+		_exit (1);
+	    }
+	    close (pipe2[1]);
 
-	    if(out)
-		fclose(out);
+	    while (fgets (s2, 1024, stream2))
+	    {
+		if (out)
+		    fprintf (out, "%s", s2);
+		y2debug ("%s", s2);
+	    }
+
+	    fclose (stream2);
+	    free (s2);
+
+	    if (out)
+		fclose (out);
 
 	}
-	/* child process */
-	else {
+	else
+	{
+	    /* child process */
 
-	    dup2(pipe2[1],1);
-	    close(pipe2[0]);
-	    close(pipe2[1]);
+	    dup2 (pipe2[1], 1);
+	    close (pipe2[0]);
+	    close (pipe2[1]);
 
-	    ret = system(command.c_str());
-	    if(WIFEXITED(ret)) ret = WEXITSTATUS(ret);
-	    else ret = WTERMSIG(ret)+128;
+	    ret = system (command.c_str ());
+	    if (WIFEXITED (ret))
+		ret = WEXITSTATUS (ret);
+	    else
+		ret = WTERMSIG (ret) + 128;
 
-	    y2debug("Exit status is %d",ret);
+	    y2debug ("Exit status is %d", ret);
 
-	    if (!tempdir.empty ()) {
-		FILE *ex = fopen((tempdir+"/exit").c_str(), "w");
-		if(ex) { fprintf(ex, "%d\n", ret); fclose(ex); }
+	    if (!tempdir.empty ())
+	    {
+		FILE *ex = fopen ((tempdir + "/exit").c_str (), "w");
+		if (ex)
+		{
+		    fprintf (ex, "%d\n", ret);
+		    fclose (ex);
+		}
 	    }
 
-	    exit(ret);
+	    _exit (ret);
 	}
 
-	waitpid(child, &ret, 0);
-	if(WIFEXITED(ret)) exit(WEXITSTATUS(ret));
-	exit(WTERMSIG(ret)+128);
+	waitpid (child, &ret, 0);
+	if (WIFEXITED (ret))
+	    _exit (WEXITSTATUS (ret));
+	_exit (WTERMSIG (ret) + 128);
     }
 
-    waitpid(log2, &ret, 0);
+    waitpid (log2, &ret, 0);
 
     y2debug ("shellcommand end");
 
-    if(WIFEXITED(ret)) return WEXITSTATUS(ret);
-    return WTERMSIG(ret)+128;
+    if (WIFEXITED (ret))
+	return WEXITSTATUS (ret);
+    return WTERMSIG (ret) + 128;
 }
 
 
 /**
  * Execute shell command on background
  */
-int shellcommand_background(const string &command)
+int
+shellcommand_background (const string &command)
 {
     /* fork the child */
-    fflush(0);
-    pid_t child = fork();
-    if(child == -1) {
-	y2error("fork failed");
+    fflush (0);
+    pid_t child = fork ();
+    if (child == -1)
+    {
+	y2error ("fork failed");
 	return -1;
     }
 
-    /* child process */
-    if(!child) {
-	shellcommand(command, "");
-	exit(0);
+    if (!child)
+    {
+	/* child process */
+
+	shellcommand (command, "");
+	_exit (0);
     }
 
     /* return success */
@@ -202,22 +231,23 @@ int shellcommand_background(const string &command)
  *
  */
 
-int main()
+int
+main ()
 {
     int r = 123456;
-    fprintf(stdout,"Oret=%d\n",r);
-    fprintf(stderr,"Eret=%d\n",r);
+    fprintf (stdout, "Oret=%d\n", r);
+    fprintf (stderr, "Eret=%d\n", r);
 
-    y2setLogfileName("-");
-    y2debug("START");
+    y2setLogfileName ("-");
+    y2debug ("START");
 
     //r = shellcommand_background("sleep 5; echo x1x; echo y2y >&2; exit 3;");
     //r = shellcommand("echo x1x; echo y2y >&2; exit 3;", "/tmp");
-    r = shellcommand("echo x1x; echo y2y >&2; exit 3;");
-    y2debug("--ret=%d",r);
+    r = shellcommand ("echo x1x; echo y2y >&2; exit 3;");
+    y2debug ("--ret=%d", r);
 
-    fprintf(stdout,"Oret=%d\n",r);
-    fprintf(stderr,"Eret=%d\n",r);
+    fprintf (stdout, "Oret=%d\n", r);
+    fprintf (stderr, "Eret=%d\n", r);
     return r;
 }
 
--- agent-system/src/ShellCommand.h
+++ agent-system/src/ShellCommand.h
@@ -8,19 +8,19 @@
  * $Id$
  */
 
-#ifndef ShellCommand_h                                                           
-#define ShellCommand_h                                                           
+#ifndef ShellCommand_h
+#define ShellCommand_h
 
 #include <string>
 
 /**
  * Execute shell command and feed its output to y2log
  */
-int shellcommand(const string &command, const string &tempdir = "");
+int shellcommand (const string &command, const string &tempdir = "");
 
 /**
  * Execute shell command on background
  */
-int shellcommand_background(const string &command);
+int shellcommand_background (const string &command);
 
 #endif /* ShellCommand_h */
