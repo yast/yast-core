f5947a6b4550b5a9cf99ab298920621df4734e9d
Martin Vidner <mvidner@suse.cz>
2004-01-09 14:47:20 +0000

    Trying advanced stuff to save typing
    svn path=/trunk/core/; revision=13166

--- agents-perl/lib/YaST/SCRAgent.pm
+++ agents-perl/lib/YaST/SCRAgent.pm
@@ -23,31 +23,34 @@ package YaST::SCRAgent;
 use strict;
 use ycp;
 
+# used when Write directly modifies a variable
+our $modified = 0;
+
 sub Read ()
 {
-    my $class = shift;
-    y2error ("Read not implemented in this agent");
+    my ($class, $path, @rest) = @_;
+    y2error ("Read not implemented in this agent for $path");
     return undef;
 }
 
 sub Write ()
 {
-    my $class = shift;
-    y2error ("Write not implemented in this agent");
+    my ($class, $path, @rest) = @_;
+    y2error ("Write not implemented in this agent for $path");
     return 0;
 }
 
 sub Dir ()
 {
-    my $class = shift;
-    y2error ("Dir not implemented in this agent");
+    my ($class, $path, @rest) = @_;
+    y2error ("Dir not implemented in this agent for $path");
     return [];
 }
 
 sub Execute ()
 {
-    my $class = shift;
-    y2error ("Execute not implemented in this agent");
+    my ($class, $path, @rest) = @_;
+    y2error ("Execute not implemented in this agent for $path");
     return undef;
 }
 
@@ -75,7 +78,7 @@ sub Run ()
 	}
 
 	my ($command, @arguments) = ycp::ParseTerm ($_);
-
+	my $ret;
 
 	if ($command eq "result")
 	{
@@ -105,8 +108,84 @@ sub Run ()
 		else
 		{
 		    my $path = $$pathref;
-		    # call it
-		    my $ret = $class->$command($path, @arguments);
+		    # try if the sub $command_$path exists
+		    my $sym = "$command$path";
+		    $sym =~ s/\./_/g;
+		    # UNIVERSAL::can
+		    if ($class->can ($sym))
+		    {
+			# call it without the path
+			$ret = $class->{$sym}(@arguments);
+		    }
+		    else
+		    {
+			no strict; # fatal error otherwise about wrong types
+
+			# try if we can Read/Write an "our" variable directly
+			# test if all this really works
+			my $can_var = 0;
+
+			# get the symbol table of the package
+			*stash = *{"${class}::"};
+			if (exists $stash{$sym})
+			{
+			    *symglob = $stash{$sym};
+			    if ($command eq "Read")
+			    {
+				$can_var = 1;
+				if (defined (%symglob))
+				{
+				    $ret = \%symglob;
+				}
+				elsif (defined (@symglob))
+				{
+				    $ret = \@symglob;
+				}
+				elsif (defined ($symglob))
+				{
+				    $ret = $symglob;
+				}
+				else
+				{
+				    y2error ("Not a scalar/array/hash: $sym");
+				    $can_var = 0;
+				}
+			    }
+			    elsif ($command eq "Write")
+			    {
+				# todo test if arg exists
+				my $arg = $arguments[0];
+				$can_var = 1;
+
+				if (defined (%symglob))
+				{
+				    %symglob = %{$arg};
+				    $modified = 1;
+				}
+				elsif (defined (@symglob))
+				{
+				    @symglob = @{$arg};
+				    $modified = 1;
+				}
+				elsif (defined ($symglob))
+				{
+				    $symglob = $arg;
+				    $modified = 1;
+				}
+				else
+				{
+				    y2error ("Not a scalar/array/hash: $sym");
+				    $can_var = 0;
+				}
+			    }
+			}
+
+			if (!$can_var)
+			{
+			    # call it the ordinary way
+			    $ret = $class->$command($path, @arguments);
+			}
+		    }
 		    if ($command eq "Write")
 		    {
 			$ret = ycp::to_bool ($ret);
@@ -117,7 +196,7 @@ sub Run ()
 	}
 	else
 	{
-	    my $ret = $class->OtherCommand ($command, @arguments);
+	    $ret = $class->OtherCommand ($command, @arguments);
 	    ycp::Return ($ret, 1);
 	}
 
