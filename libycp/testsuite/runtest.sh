b0e24bc40b29ed63f5448692160646600e7b0266
Michal Švec <msvec@suse.cz>
2004-02-03 13:04:16 +0000

    - filter out the Electric Fence greeting
    svn path=/trunk/core/; revision=13807

--- libycp/testsuite/runtest.sh
+++ libycp/testsuite/runtest.sh
@@ -4,5 +4,5 @@ unset Y2DEBUG
 unset Y2DEBUGALL
 unset Y2DEBUGGER
 
-(./runycp -l - -I tests/Include -M tests/Module $1 >$2) 2>&1 | fgrep -v " <0> " | grep -v "^$" | sed 's/^....-..-.. ..:..:.. [^)]*) //g' > $3
+(./runycp -l - -I tests/Include -M tests/Module $1 >$2) 2>&1 | fgrep -v 'Electric Fence' | fgrep -v " <0> " | grep -v "^$" | sed 's/^....-..-.. ..:..:.. [^)]*) //g' > $3
 exit 0
