ffcaaf745a90157df88c6a9d030480c29493f26a
Klaus Kämpf <kkaempf@suse.de>
2004-02-29 16:28:10 +0000

    coding style (in preparation of #34888)
    svn path=/trunk/core/; revision=14961

--- agent-resolver/src/ResolverAgent.cc
+++ agent-resolver/src/ResolverAgent.cc
@@ -68,9 +68,12 @@ static const info_entry headers[] = {
 static const char *findKey (const char *tag)
 {
     int i = 0;
-    while (headers[i].key != 0) {
+    while (headers[i].key != 0)
+    {
         if (strncmp (tag, headers[i].tag, strlen (headers[i].tag)) == 0)
+	{
             return headers[i].key;
+	}
         i = i + 1;
     }
 
@@ -81,9 +84,12 @@ static const char *findKey (const char *tag)
 bool allowedInfoKey (const char *key)
 {
     int i = 0;
-    while (headers[i].key != 0) {
+    while (headers[i].key != 0)
+    {
         if (key && (strcmp (key, headers[i].key) == 0))
+	{
             return true;
+	}
         i = i + 1;
     }
 
@@ -94,10 +100,14 @@ bool allowedInfoKey (const char *key)
 static char *nextWhitespace (const char *lptr)
 {
     if (lptr == 0)
+    {
 	return 0;
+    }
 
     while (*lptr && !isspace (*lptr))
+    {
 	lptr++;
+    }
 
     return (char *)lptr;
 }
@@ -106,10 +116,14 @@ static char *nextWhitespace (const char *lptr)
 static char *eatWhitespaces (const char *lptr)
 {
     if (lptr == 0)
+    {
 	return 0;
+    }
 
     while (*lptr && isspace (*lptr))
+    {
 	lptr++;
+    }
 
     return (char *)lptr;
 }
@@ -143,7 +157,8 @@ static int fillCache (const char *filename)
 
     localCache = YCPMap();	// initialize with empty map
 
-    while (fgets (lbuf, lbufsize, f) != 0) {
+    while (fgets (lbuf, lbufsize, f) != 0)
+    {
 	y2debug ("fread(%s)", lbuf);
 
 	lptr = strchr (lbuf, '#');	// process comment
@@ -157,8 +172,9 @@ static int fillCache (const char *filename)
             if (strncmp (lbuf, "### END INFO", 12) == 0)
             {
                 if (last_key)   // one more key to process
+		{  
                     localCache->add (YCPString (last_key), YCPString ((info_buf.substr (0, info_buf.length () - 1)).c_str ()));
-
+		}
                 if (!processing_info) y2warning ("End of info without beggining!");
                 processing_info = false;
                 continue;
@@ -166,32 +182,38 @@ static int fillCache (const char *filename)
             if (const char *key = findKey (lbuf))
             {
                 if (last_key)
+		{
                     localCache->add (YCPString (last_key), YCPString ((info_buf.substr (0, info_buf.length () - 1)).c_str ()));
+		}
 
                 if (char *info = strchr (lbuf, ':'))
+		{
                     info_buf = eatWhitespaces (strlen (info) > 0 ? info + 1 : info);
-
+		}
                 last_key = key;
                 continue;
             }
             else if (processing_info)
+	    {
                 info_buf = info_buf + lbuf; // keep the other lines with '#' at the begging and '\n' in the end
-
+	    }
 	    *lptr = 0;          // don't care about other comments
         }
 
 	if (lbuf[0] == '\0'		// skip other comment lines
 	    || lbuf[0] == '\n')
+	{
 	    continue;
-
+	}
 	lptr = nextWhitespace (lbuf);	// break line at first whitespace
 	y2debug ("next(%s)", lptr);
 
 	if (lptr == 0
 	    || *lptr == '\n'
 	    || *lptr == 0)			// skip lines without <key><ws><value> syntax
+	{
 	    continue;
-
+	}
 	*lptr++ = 0;
 
 	YCPValue value = YCPNull();
@@ -205,18 +227,22 @@ static int fillCache (const char *filename)
 
 	if ((strcmp  (lbuf, "search") == 0)
 	   || (strcmp  (lbuf, "sortlist") == 0)
-	   || (strcmp  (lbuf, "options") == 0)) {
+	   || (strcmp  (lbuf, "options") == 0))
+	{
 
 	    y2debug ("list of strings '%s'", lbuf);
 
 	    YCPList ret;
 
-	    for (;;) {
+	    for (;;)
+	    {
 		vptr = lptr;
 		lptr = nextWhitespace (vptr);
 		if ((lptr == 0)
 		    || (lptr == vptr))
+		{
 		    break;
+		}
 		*lptr++ = 0;
 		ret->add (YCPString (vptr));
 	    }
@@ -226,13 +252,16 @@ static int fillCache (const char *filename)
 
 	// single value, but multiple lines allowed
 
-	else if (strcmp (lbuf, "nameserver") == 0) {
+	else if (strcmp (lbuf, "nameserver") == 0)
+	{
 
 	    y2debug ("multiple lines '%s':'%s'", lbuf, vptr);
 
 	    value = localCache->value (key);
 	    if (value.isNull())
+	    {
 		value = YCPList();
+	    }
 	    lptr = nextWhitespace (vptr);
 	    *lptr = 0;
 	    value = value->asList()->functionalAdd (YCPString (vptr));
@@ -240,7 +269,8 @@ static int fillCache (const char *filename)
 
 	// single value
 
-	else if (strcmp (lbuf, "domain") == 0) {
+	else if (strcmp (lbuf, "domain") == 0)
+	{
 
 	    y2debug ("single value '%s'", lbuf);
 
@@ -248,14 +278,16 @@ static int fillCache (const char *filename)
 	    *lptr = 0;
 	    value = YCPString (vptr);
 	}
-	else {
+	else
+	{
 	    y2warning ("key '%s' not recognized", lbuf);
 	}
 
 	localCache->add (key, value);
     }
 
-    if (!feof (f)) {
+    if (!feof (f))
+    {
 	y2error ("Can't completely read %s", filename);
 	retval = -1;
     }
@@ -273,12 +305,17 @@ static int fillCache (const char *filename)
 static int flushCache (const char *filename)
 {
     if (!cacheValid)
+    {
 	return 0;
+    }
     if (!cacheDirty)
+    {
 	return 0;
+    }
 
     FILE* f = fopen (filename, "w+");
-    if (f == 0) {
+    if (f == 0)
+    {
 	y2error ("Can't open %s for writing", filename);
 	return -1;
     }
@@ -295,13 +332,19 @@ static int flushCache (const char *filename)
 	{
 	    info = localCache->value (YCPString (headers[i].key));
 	    if (info.isNull () || info->isVoid ())
+	    {
 		y2warning ("Info key %s not found!", headers[i].key);
+	    }
 	    else
 	    {
 		if (info->isString ())
+		{
 		    fprintf (f, "%s %s\n", headers[i].tag, info->asString ()->value_cstr ());
+		}
 		else
+		{
 		    y2error ("Wrong type for info key %s!", headers[i].tag);
+		}
 	    }
 	    i = i + 1;
 	}
@@ -311,9 +354,11 @@ static int flushCache (const char *filename)
     int retval = 0;
     YCPMapIterator mptr = localCache->begin();
 
-    while (mptr != localCache->end()) {
+    while (mptr != localCache->end())
+    {
 	YCPValue key = mptr.key();
-	if (key.isNull() || !key->isString()) {
+	if (key.isNull() || !key->isString())
+	{
 	    y2error ("Bad key in localCache");
 	    retval = -1;
 	    break;
@@ -323,35 +368,44 @@ static int flushCache (const char *filename)
 
 	if ((skey == "search")
 	    || (skey == "sortlist")
-	    || (skey == "options")) {
+	    || (skey == "options"))
+	{
 
-	    if (value.isNull() || !value->isList()) {
+	    if (value.isNull() || !value->isList())
+	    {
 		y2error ("Bad value for key '%s'", skey.c_str());
 		break;
 	    }
 	    YCPList list = value->asList();
-	    if (list->size() < 0) {
+	    if (list->size() < 0)
+	    {
 		y2error ("Bad list size for key '%s'", skey.c_str());
 		break;
 	    }
-	    if (list->size() == 0) {
+	    if (list->size() == 0)
+	    {
 		break;
 	    }
 	    fprintf (f, skey.c_str());
 	    int i = 0;
-	    for (i = 0; i < list->size(); i++) {
+	    for (i = 0; i < list->size(); i++)
+	    {
 		if (list->value(i).isNull()
-		    || !list->value(i)->isString()) {
+		    || !list->value(i)->isString())
+		{
 		    y2error ("Skipping bad list element for key '%s'", skey.c_str());
 		}
-		else {
+		else
+		{
 		    fprintf (f, " %s", list->value(i)->asString()->value().c_str());
 		}
 	    }
 	    fprintf (f, "\n");
 	}
-	else if (skey == "nameserver") {
-	    if (value.isNull() || !value->isList()) {
+	else if (skey == "nameserver")
+	{
+	    if (value.isNull() || !value->isList())
+	    {
 		y2error ("Bad value for key '%s'", skey.c_str());
 		break;
 	    }
@@ -373,18 +427,22 @@ static int flushCache (const char *filename)
 		}
 	    }
 	}
-	else if (skey == "domain") {
-	    if (value.isNull() || !value->isString()) {
+	else if (skey == "domain")
+	{
+	    if (value.isNull() || !value->isString())
+	    {
 		y2error ("Bad value for key '%s'", skey.c_str());
 		break;
 	    }
             if (value->asString ()->value ().size () > 0)
                 fprintf (f, "%s %s\n", skey.c_str(), value->asString()->value().c_str());
 	}
-        else if (allowedInfoKey (skey.c_str ())) {
+        else if (allowedInfoKey (skey.c_str ()))
+	{
             y2debug ("Skipping info key '%s'", skey.c_str());
         }
-	else {
+	else
+	{
 	    y2error ("Skipping invalid key '%s'", skey.c_str());
 	}
 	mptr++;
@@ -399,9 +457,12 @@ static int flushCache (const char *filename)
 static int allowedKey (const char *key)
 {
     int i = 0;
-    while (resolver_keys[i] != 0) {
+    while (resolver_keys[i] != 0)
+    {
 	if (strcmp (key, resolver_keys[i]) == 0)
+	{
 	    return 0;
+	}
 	i = i + 1;
     }
     return -1;
@@ -418,7 +479,9 @@ ResolverAgent::ResolverAgent () : file_name ("/etc/resolv.conf")
 ResolverAgent::~ResolverAgent ()
 {
     if (cacheValid && cacheDirty)
+    {
 	flushCache(file_name.c_str ());
+    }
 }
 
 
@@ -429,13 +492,16 @@ ResolverAgent::Read (const YCPPath& path, const YCPValue& arg, const YCPValue& o
 
     fillCache (file_name.c_str ());
 
-    if (path->isRoot()) {
+    if (path->isRoot())
+    {
 	return localCache;
     }
 
     YCPValue ret = localCache->value (YCPString (path->component_str(0)));
     if (ret.isNull ())
+    {
 	return YCPVoid ();
+    }
     return ret;
 }
 
@@ -452,7 +518,9 @@ ResolverAgent::Write (const YCPPath& path, const YCPValue& value,
     if (path->isRoot())
     {
 	if (value.isNull() || value->isVoid())
+	{
 	    return YCPBoolean (flushCache (file_name.c_str ()) == 0);
+	}
 	if (!value->isMap())
 	{
 	    ycp2error ("Bad value to Write (.resolver)");
@@ -469,7 +537,8 @@ ResolverAgent::Write (const YCPPath& path, const YCPValue& value,
 	}
         else if (strcmp (key, "write_header") == 0)
 	{
-            if (value->isBoolean ()) {
+            if (value->isBoolean ())
+	    {
                 writeHeader = value->asBoolean ()->value ();
             }
             else
@@ -499,9 +568,11 @@ YCPList ResolverAgent::Dir (const YCPPath& path)
 {
     YCPList retval;
 
-    if (path->isRoot()) {
+    if (path->isRoot())
+    {
 	int i = 0;
-	while (resolver_keys[i] != 0) {
+	while (resolver_keys[i] != 0)
+	{
 	    retval->add (YCPString (resolver_keys[i]));
 	    i = i + 1;
 	}
@@ -514,8 +585,10 @@ YCPValue ResolverAgent::otherCommand (const YCPTerm& term)
     string fname = "/etc/resolv.conf";
 
     string symbol = term->name ();
-    if (symbol == "ResolverAgent" && term->size () == 1) {
-	if (term->value(0)->isString()) {
+    if (symbol == "ResolverAgent" && term->size () == 1)
+    {
+	if (term->value(0)->isString())
+	{
     	    fname = term->value (0)->asString ()->value ();
             y2debug ("resolving file now: %s", fname.c_str ());
 	    cacheValid = false;
