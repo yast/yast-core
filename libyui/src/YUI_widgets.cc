7bc3cb332f8a238dda8ffe275f119223e3e574e2
Stefan Hundhammer <sh@suse.de>
2004-03-09 14:31:32 +0000

    Make sure error logging appears even without weird configuration
    svn path=/trunk/core/; revision=15790

--- libyui/src/YUI_widgets.cc
+++ libyui/src/YUI_widgets.cc
@@ -2422,11 +2422,16 @@ YWidget * YUI::widgetWithId( const YCPValue & id, bool log_error )
 	YWidget *widget = currentDialog()->findWidget( id );
 	if ( widget ) return widget;
 	if ( log_error )
+	{
+	    y2error( "No widget with `" YUISymbol_id "( %s )", id->toString().c_str() );
 	    ycperror( "No widget with `" YUISymbol_id "( %s )", id->toString().c_str() );
+	}
     }
     else if ( log_error )
-	ycperror( "No dialog existing, therefore no widget with `" YUISymbol_id "( %s )",
-		 id->toString().c_str() );
+    {
+	y2error( "No dialog existing, therefore no widget with `" YUISymbol_id "( %s )", id->toString().c_str() );
+	ycperror( "No dialog existing, therefore no widget with `" YUISymbol_id "( %s )", id->toString().c_str() );
+    }
 
     return 0;
 }
@@ -2438,11 +2443,16 @@ YWidget * YUI::widgetWithId( YContainerWidget *widgetRoot, const YCPValue & id, 
 	YWidget *widget = widgetRoot->findWidget( id );
 	if ( widget ) return widget;
 	if ( log_error )
+	{
+	    y2error( "No widget with `" YUISymbol_id "( %s )", id->toString().c_str() );
 	    ycperror( "No widget with `" YUISymbol_id "( %s )", id->toString().c_str() );
+	}
     }
     else if ( log_error )
-	ycperror( "No dialog existing, therefore no widget with `" YUISymbol_id "( %s )",
-		 id->toString().c_str() );
+    {
+	y2error( "No dialog existing, therefore no widget with `" YUISymbol_id "( %s )", id->toString().c_str() );
+	ycperror( "No dialog existing, therefore no widget with `" YUISymbol_id "( %s )", id->toString().c_str() );
+    }
 
     return 0;
 }
