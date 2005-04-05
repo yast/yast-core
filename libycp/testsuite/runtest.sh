d3813d807d4806605f88d4d80ddfb4ffe7b0e44e
Stanislav Višňovský <visnov@suse.cz>
2005-04-05 15:19:02 +0000

    Enable ElectricFence again without checking for 0 size allocs
    svn path=/trunk/core/; revision=22919

--- libycp/testsuite/runtest.sh
+++ libycp/testsuite/runtest.sh
@@ -4,5 +4,7 @@ unset Y2DEBUG
 unset Y2DEBUGALL
 unset Y2DEBUGGER
 
+export EF_ALLOW_MALLOC_0=1
+
 (./runycp -l - -I tests/Include -M tests/Module $1 >$2) 2>&1 | fgrep -v 'Electric Fence' | fgrep -v " <0> " | grep -v "^$" | sed 's/^....-..-.. ..:..:.. [^)]*) //g' > $3
 exit 0
