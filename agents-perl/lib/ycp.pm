264ac3d01f43fb2a138e4e8ea3e425a33511ba7d
Martin Vidner <mvidner@suse.cz>
2006-08-21 14:17:28 +0000

    ycp.pm: added PathComponents to normalize nontrivial paths, to be used in ag_etc_sudoers for example
    svn path=/branches/SuSE-SLE-10-SP1-Branch/core/; revision=32464

--- agents-perl/lib/ycp.pm
+++ agents-perl/lib/ycp.pm
@@ -85,6 +85,7 @@ use Sys::Hostname;
 my @e_io = qw(
 	       ParseTerm
 	       ParseCommand
+	       PathComponents
 	       Return
 	       );
 
@@ -412,6 +413,52 @@ sub ParseYcpString ($)
 	    return ("", "Can't happen in ParseYcpString", $ycp_value);
 	}
     }
+}
+
+# Converts a path (a string reference) to a list of its components
+# Normalizes (removes superfluous quoting)
+# . -> ()
+# .foo.bar -> ('foo', 'bar')
+# ."foo" -> ('foo')
+# ."double\"quote" -> ('double"quote')
+# ."a.dot" -> ('a.dot')
+# On error, returns undef
+sub PathComponents ($)
+{
+    my $path_ref = shift;
+    if (ref ($path_ref) ne "SCALAR") {
+	y2error ("Expecting a reference to a scalar");
+	return undef;
+    }
+    my $path = $$path_ref;
+
+    return undef if $path eq "";
+    return () if $path eq ".";
+
+    my @result = ();
+
+    while ($path =~ s/^\.(${lex_pathsegment})(.*)/$2/o) {
+	my $segment = $1;
+	if ($segment =~ /^"/) {
+	    # FIXME check whether paths are like strings, unify
+	    my ($parsed, $err, $rest) = ParseYcpString ($segment);
+	    if ($err ne "") {
+		y2error ("Bad complex path component: '$err'");
+		return undef;
+	    }
+	    elsif ($rest ne "") {
+		y2error ("Extra characters in path component: '$rest'");
+		return undef;
+	    }
+	    $segment = $parsed;
+	}
+	push @result, $segment;
+    }
+    if ($path ne "") {
+	y2error ("Extra characters in path: '$path'");
+	return undef;
+    }
+    return @result;
 }
 
 # Internal
