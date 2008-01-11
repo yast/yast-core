ac0a64f5fa4466fd7de6fe521d18720c853dee34
Stefan Hundhammer <sh@suse.de>
2008-01-11 17:43:09 +0000

    separating UI logging
    svn path=/trunk/core/; revision=43565

---
+++ libyui/src/YUILog.h
@@ -0,0 +1,141 @@
+/*---------------------------------------------------------------------\
+|								       |
+|		       __   __	  ____ _____ ____		       |
+|		       \ \ / /_ _/ ___|_   _|___ \		       |
+|			\ V / _` \___ \ | |   __) |		       |
+|			 | | (_| |___) || |  / __/		       |
+|			 |_|\__,_|____/ |_| |_____|		       |
+|								       |
+|				core system			       |
+|							 (C) SuSE GmbH |
+\----------------------------------------------------------------------/
+
+  File:		YUILog.h
+
+  Author:     	Stefan Hundhammer <sh@suse.de>
+
+/-*/
+
+#ifndef YUILog_h
+
+#ifndef YUILogComponent
+#error Missing   #define YUILogComponent "myComponent"
+#endif
+
+#include <iosfwd>
+#include <iostream>
+
+//
+// Macros for Application use.
+//
+// They all return a std::ostream &  for use with operator<<().
+// #define YUILogComponent before including this header file.
+//
+//    #define YUILogComponent "myComponent"
+//    #include <YUILog.h>
+//
+//    ...
+//    yuiDebug() << "Creating widget" << widget << endl;
+//    yuiError() << "No widget with ID " << id << endl;
+//
+// Milestone, Warning and Error are always logged, Debug only when enabled.
+//
+
+#define yuiDebug()	YUILog::debug    ( YUILogComponent, __FILE__, __LINE__, __FUNCTION__ )
+#define yuiMilestone()	YUILog::milestone( YUILogComponent, __FILE__, __LINE__, __FUNCTION__ )
+#define yuiWarning()	YUILog::warning  ( YUILogComponent, __FILE__, __LINE__, __FUNCTION__ )
+#define yuiError()	YUILog::error    ( YUILogComponent, __FILE__, __LINE__, __FUNCTION__ )
+
+
+class YUILogPrivate;
+
+enum YUILogLevel_t
+{
+    YUI_LOG_DEBUG     = 0,
+    YUI_LOG_MILESTONE,
+    YUI_LOG_WARNING,
+    YUI_LOG_ERROR
+};
+
+
+/**
+ * UI logging.
+ **/
+class YUILog
+{
+public:
+    
+    /**
+     * Logging functions for each log level. They all access the singleton object for this class.
+     * This means that the first call to any of those functions will create the singleton YUILog object.
+     **/
+    static std::ostream & debug    ( const char * logComponent, const char * sourceFileName, int lineNo, const char * functionName );
+    static std::ostream & milestone( const char * logComponent, const char * sourceFileName, int lineNo, const char * functionName );
+    static std::ostream & warning  ( const char * logComponent, const char * sourceFileName, int lineNo, const char * functionName );
+    static std::ostream & error    ( const char * logComponent, const char * sourceFileName, int lineNo, const char * functionName );
+
+    /**
+     * Return the singleton object for this class.
+     * This will create the singleton if it doesn't exist yet.
+     **/
+    static YUILog * instance();
+
+    /**
+     * Enable or disable debug logging.
+     **/
+    static void enableDebugLogging( bool debugLogging = true );
+
+    /**
+     * Return 'true' if debug logging is enabled, 'false' if not.
+     **/
+    static bool debugLoggingEnabled();
+
+    
+    typedef void (*YUILoggerFunction)( YUILogLevel_t,	// logLevel
+				       const char *,	// logComponent
+				       const char *,	// sourceFileName
+				       int 	 	// sourceLineNo
+				       const char * 	// sourceFunctionName
+				       const char * );	// message
+
+    /**
+     * Set the UI logger function. This is the function that will ultimately
+     * receive all UI log output (except debug logging if debug logging is
+     * disabled).
+     *
+     * By default, all logging is output to stdout. This behaviour can be
+     * restored if 0 is passed as a function pointer here.
+     **/
+    static void setLoggerFunction( YUILoggerFunction );
+
+    /**
+     * Return the UI logger function. 0 means stdout.
+     **/
+    static YUILoggerFunction loggerFunction();
+    
+    
+private:
+    /**
+     * Constructor.
+     *
+     * Not for application use - use one of the static functions above instead.
+     * They all access the singleton object for this class.
+     **/
+    YUILog();
+
+    /**
+     * Destructor.
+     **/
+    ~YUILog();
+
+    //
+    // Data
+    //
+    
+    ImplPtr<YUILogPrivate> priv;
+};
+
+
+#define YUILog_h
+
+#endif // YUILog_h
