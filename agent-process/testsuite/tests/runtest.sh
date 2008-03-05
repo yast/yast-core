8706c9677a5c38acdf29763c2b7c58863587f235
Lukáš Ocilka <locilka@suse.cz>
2008-03-05 11:21:38 +0000

    - Adjusted and fixed process agent testsuite.
    svn path=/trunk/core/; revision=45206

--- agent-process/testsuite/tests/runtest.sh
+++ agent-process/testsuite/tests/runtest.sh
@@ -7,4 +7,10 @@ unset Y2DEBUGGER
 export Y2DEBUGSHELL=1
 
 # ignore also milestones (they are not constant and logged by liby2util - cannot be simply turned off)
-(./run_ag_process -l - $1 >$2) 2>&1 | fgrep -v -e " <0> " -e " <1> " | grep -v "^$" | sed 's/^....-..-.. ..:..:.. [^)]*) //g' > $3
+# ignore also line numbers (any change in the core might make the testsuite fail)
+(./run_ag_process -l - $1 >$2) 2>&1 | \
+    fgrep -v -e " <0> " -e " <1> " | \
+    grep -v "^$" | \
+    sed 's/^....-..-.. ..:..:.. [^)]*) //g' | \
+    sed 's/^\(\[[^]]\+\] [^(]\+([^)]\+)\):[0-9]\+ \(.*\)/\1 \2/g' \
+    > $3
