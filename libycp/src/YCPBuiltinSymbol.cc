e9c7027678f371f8de8228f52d85cdb9c0b64d53
Arvin Schnell <aschnell@suse.de>
2008-12-21 20:11:51 +0000

    - added conversion of integer to symbol
    svn path=/trunk/core/; revision=54322

--- libycp/src/YCPBuiltinSymbol.cc
+++ libycp/src/YCPBuiltinSymbol.cc
@@ -21,6 +21,7 @@
 #include "ycp/YCPBuiltinSymbol.h"
 #include "ycp/YCPSymbol.h"
 #include "ycp/YCPString.h"
+#include "ycp/YCPInteger.h"
 #include "ycp/YCPVoid.h"
 #include "ycp/YCPCode.h"
 #include "ycp/YCPCodeCompare.h"
@@ -32,9 +33,10 @@ extern StaticDeclaration static_declarations;
 
 
 static YCPValue
-s_tosymbol (const YCPString& v)
+s_tosymbol1 (const YCPString& v)
 {
-    /* @builtin tosymbol 
+    /* @builtin tosymbol
+     * @id tosymbol-string
      * @short Converts a string to a symbol.
      *
      * @param string VALUE
@@ -47,11 +49,29 @@ s_tosymbol (const YCPString& v)
 }
 
 
+static YCPValue
+s_tosymbol2 (const YCPInteger& v)
+{
+    /* @builtin tosymbol
+     * @id tosymbol-integer
+     * @short Converts a integer to a symbol.
+     *
+     * @param string VALUE
+     * @return symbol
+     *
+     * @usage tosymbol(69) -> `69
+     */
+
+    return YCPSymbol(v->toString());
+}
+
+
 YCPBuiltinSymbol::YCPBuiltinSymbol ()
 {
     // must be static, registerDeclarations saves a pointer to it!
     static declaration_t declarations[] = {
-	{ "tosymbol",	"symbol (const string)",				(void*) s_tosymbol },
+	{ "tosymbol",	"symbol (const string)",				(void*) s_tosymbol1 },
+	{ "tosymbol",	"symbol (const integer)",				(void*) s_tosymbol2 },
 	{ 0 }
     };
 
