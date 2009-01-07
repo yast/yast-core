36cf7b064c56de0f596b90397dc7cc2b34fac5de
Arvin Schnell <aschnell@suse.de>
2009-01-07 11:08:51 +0000

    - added ycpequal functor - make ycpless and ycpequal accept parameter to respect locale
    svn path=/trunk/core/; revision=54410

--- libycp/src/include/ycp/ycpless.h
+++ libycp/src/include/ycp/ycpless.h
@@ -10,10 +10,11 @@
 |                                                        (C) SuSE GmbH |
 \----------------------------------------------------------------------/
 
-   File:       ycpless.h
+   File:	ycpless.h
 
-   Author:     Mathias Kettner <kettner@suse.de>
-   Maintainer: Thomas Roelz <tom@suse.de>
+   Authors:	Mathias Kettner <kettner@suse.de>
+		Arvin Schnell <aschnell@suse.de>
+   Maintainer:	Thomas Roelz <tom@suse.de>
 
 /-*/
 // -*- c++ -*-
@@ -23,19 +24,56 @@
 
 #include "YCPValue.h"
 
-using std::binary_function;
 
 /*
- * global comparison function to be used with STL-Containers as generic
- * ordering operator. Compares two YCPValues and returns
- *    true if the first value is less than the second one,
- *    false otherwise.
+ * Global comparison functor usable as generic ordering operator for
+ * STL-containers and as predicate for STL-algorithms.
+ *
+ * Compares two YCPValues and returns true if the first value is less than the
+ * second one, false otherwise. Optionally the comparison is locale aware.
  */
-
-struct ycpless : public binary_function<YCPValue, YCPValue, bool>
+class ycpless : public std::binary_function<YCPValue, YCPValue, bool>
 {
-    bool operator()(const YCPValue& x, const YCPValue& y)
-	const { return x->compare(y) == YO_LESS; };
+
+public:
+
+    ycpless(bool respect_locale = false) : respect_locale(respect_locale) {}
+
+    bool operator()(const YCPValue& x, const YCPValue& y) const
+    {
+	return x->compare(y, respect_locale) == YO_LESS;
+    }
+
+private:
+
+    const bool respect_locale;
+
 };
+
+
+/*
+ * Global comparison functor usable as predicate for STL-algorithms.
+ *
+ * Compares two YCPValues and returns true if they are equal, false otherwise.
+ * Optionally the comparison is locale aware.
+ */
+class ycpequal : public std::binary_function<YCPValue, YCPValue, bool>
+{
+
+public:
+
+    ycpequal(bool respect_locale = false) : respect_locale(respect_locale) {}
+
+    bool operator()(const YCPValue& x, const YCPValue& y) const
+    {
+	return x->compare(y, respect_locale) == YO_EQUAL;
+    }
+
+private:
+
+    const bool respect_locale;
+
+};
+
 
 #endif   // ycpless_h
