6506a1533895a84751187f740ae49749fb1d78fb
Martin Vidner <mvidner@suse.cz>
2009-04-15 12:49:54 +0000

    Added an example of YaPI::USERS call. (--explicit makes core segfault, yum!)
    Moved polkit helpers to separate file.
    
    svn path=/trunk/core/; revision=56790

--- dbus/namespace_service/doc/dbus_samba_export.py
+++ dbus/namespace_service/doc/dbus_samba_export.py
@@ -2,10 +2,9 @@
 
 # a simple example showing how to integrate Yast Dbuse service into KDE desktop
 
+from polkit_helper import retry # in .
 import dbus
 import sys
-import os
-import re
 
 def main():
 
@@ -35,38 +34,8 @@ def export_dir(directory):
 
 	call = lambda: samba.AddShare(share_name, {'path':directory, 'comment':'Exported directory ' + directory, 'read only':'Yes'},
 				      dbus_interface='org.opensuse.YaST.Values')
-	result = polkit_retry(call)
+	result = retry(call)
 	print "Result:", result
-
-def polkit_retry(alambda):
-	try:
-	    result = alambda()
-	except dbus.exceptions.DBusException, e:
-		if e.get_dbus_name() == 'org.freedesktop.PolicyKit.Error.NotAuthorized':
-		    message = e.get_dbus_message()
-
-		    # get result and action name
-		    parts = re.split(' +', message)
-		    
-		    if len(parts) == 2 and re.match("auth_.*", parts[1]):
-			    print 'Authorization ' + parts[0] + ' is needed'
-			    polkit_res = obtain_authorization(parts[0])
-			    print 'Authorization obtained: ' + str(polkit_res)
-
-			    result = alambda()
-		    else:
-			print 'PolicyKit error: ' + message
-			raise
-		else:
-		    raise
-	return result
-
-def obtain_authorization(action_id):
-	session_bus = dbus.SessionBus()
-	window_id = 0
-	polkit_obj = session_bus.get_object('org.freedesktop.PolicyKit.AuthenticationAgent', '/');
-	polkit_res = polkit_obj.ObtainAuthorization(action_id, window_id, os.getpid(), dbus_interface='org.freedesktop.PolicyKit.AuthenticationAgent')
-	return polkit_res
 
 if __name__ == "__main__":
     main()
