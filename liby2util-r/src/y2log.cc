507ff5f93de9879e9bea8369d3e67cd1780902ff
Martin Vidner <mvidner@suse.cz>
2010-02-01 16:16:46 +0000

    Fixed a crash in LogTail by adding a mutex to the shared variable (bnc#565918).
    svn path=/branches/SuSE-Code-11-SP1-Branch/core/; revision=60641

--- liby2util-r/src/y2log.cc
+++ liby2util-r/src/y2log.cc
@@ -322,7 +322,10 @@ void y2_vlogger_blanik(loglevel_t level, const char *component, const char *file
 	else
 	    tolog = y2_logfmt_prefix (level) + common;
 	// store the message for worse times
+	static pthread_mutex_t blanik_mutex = PTHREAD_MUTEX_INITIALIZER;
+	pthread_mutex_lock (&blanik_mutex);
 	blanik.push_back (tolog);
+	pthread_mutex_unlock (&blanik_mutex);
     }
 }
 
