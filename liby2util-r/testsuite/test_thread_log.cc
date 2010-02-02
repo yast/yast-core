5b5938bd840b13d36222db4af345eaf2c1694077
Martin Vidner <mvidner@suse.cz>
2010-02-02 09:03:09 +0000

    Added comments, cosmetic test case changes.
    svn path=/branches/SuSE-Code-11-SP1-Branch/core/; revision=60650

--- liby2util-r/testsuite/test_thread_log.cc
+++ liby2util-r/testsuite/test_thread_log.cc
@@ -1,5 +1,10 @@
 // adapted from
 // http://en.wikipedia.org/wiki/POSIX_Threads#Example
+
+// Test case for bnc#565918:
+// The container variable for y2debug messages was not protected from
+// concurrent thread access.
+// Simply invoke y2debug from several threads many times to make it crash.
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,21 +29,24 @@ void *PrintHello(void *threadid)
 int main (int argc, char *argv[])
 {
    pthread_t threads[NUM_THREADS];
- 
+
+   // enable collecting y2debug messages
    setenv("Y2DEBUGONCRASH", "1", 1 /*overwrite*/);
-   ticks = argv[1]? atoi(argv[1]): 1000;
+   ticks = argc>1? atoi(argv[1]): 1000;
    int rc;
    intptr_t t;
    for(t=1; t<=NUM_THREADS; t++){
-     printf("In main: creating thread %p\n", (void*)t);
+      printf("In main: creating thread %p\n", (void*)t);
       rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
  
  
       if (rc){
          printf("ERROR; return code from pthread_create() is %d\n", rc);
-         exit(-1);
+	 return 1;
       }
    }
    pthread_exit(NULL);
+   // if the bug exists, the program crashes before it can exit succesfully
+   return 0;
 }
 
