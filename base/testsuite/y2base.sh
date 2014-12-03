8a7a29c6ffc925742cb767609ad6f3e66c2cc6ac
Martin Vidner <mvidner@suse.cz>
2014-12-03 14:55:10 +0100

    y2base.sh: made more readable

--- base/testsuite/y2base.sh
+++ base/testsuite/y2base.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
-T=../..                         # Top dir
-L=src/.libs                     # Lib subdir
-AG_ANY_DIR=$T/agent-any/$L
+ROOT=../..
+LIBS=src/.libs
+AG_ANY_DIR=$ROOT/agent-any/$LIBS
+# The agent is looked for not in $Y2DIR but in $Y2DIR/plugin so create 'plugin'
 ln -snf . $AG_ANY_DIR/plugin
 export Y2DIR=$AG_ANY_DIR
-export LD_LIBRARY_PATH=$T/liby2util-r/$L:$T/libycp/$L:$T/liby2/$L:$T/libscr/$L:$T/wfm/$L:$T/scr/$L
+export LD_LIBRARY_PATH="\
+$ROOT/liby2util-r/$LIBS:\
+$ROOT/libycp/$LIBS:\
+$ROOT/liby2/$LIBS:\
+$ROOT/libscr/$LIBS:\
+$ROOT/wfm/$LIBS:\
+$ROOT/scr/$LIBS\
+"
 # ignore incompatible ruby-bindings while developing
 export Y2DISABLELANGUAGEPLUGINS=1
-../$L/y2base -l ${1%.*}.log ./$1 -S "($(pwd))" testsuite
+../$LIBS/y2base -l ${1%.*}.log ./$1 -S "($(pwd))" testsuite
